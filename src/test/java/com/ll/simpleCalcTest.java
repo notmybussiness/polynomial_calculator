7c9be995cbe88270b333870f180a34cfc451b39b
notmybussiness <cuisine97@naver.com>
2024-11-27 10:35:44 +0900

    Plus ê¸x+y°능 blue

--- src/test/java/com/ll/simpleCalcTest.java
+++ src/test/java/com/ll/simpleCalcTest.java
@@ -13,6 +13,16 @@ public class simpleCalcTest {
         int result = simpleCalculator.Plus(2,3);
 
         assertThat(result).isEqualTo(5);
-
+    }
+    @Test
+    @DisplayName("더하기 기능 여러번 테스트")
+    void t1(){
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.Plus(1,2);
+        assertThat(rs).isEqualTo(3);
+        rs = simpleCalculator.Plus(5,187);
+        assertThat(rs).isEqualTo(192);
+        rs = simpleCalculator.Plus(392,88);
+        assertThat(rs).isEqualTo(480);
     }
 }
