84d53ff1b0a6668ba48a743e7a8674e5000e238f
notmybussiness <cuisine97@naver.com>
2024-11-27 11:26:16 +0900

    곱셈 기능 R - G - B

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -5,4 +5,8 @@ public class SimpleCalculator {
     public int Plus(int num1, int num2){
         return num1+num2;
     }
+
+    public int Multiply(int num1, int num2){
+        return num1*num2;
+    }
 }
--- src/test/java/com/ll/simpleCalcTest.java
+++ src/test/java/com/ll/simpleCalcTest.java
@@ -6,23 +6,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class simpleCalcTest {
+    SimpleCalculator simpleCalculator = new SimpleCalculator();
     @Test
     @DisplayName("더하기 기능 2+3 =5 테스트")
-    public void testPlus(){
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
+    public void t1(){
+//        SimpleCalculator simpleCalculator = new SimpleCalculator();
         int result = simpleCalculator.Plus(2,3);
 
         assertThat(result).isEqualTo(5);
     }
     @Test
     @DisplayName("더하기 기능 여러번 테스트")
-    void t1(){
-        SimpleCalculator simpleCalculator = new SimpleCalculator();
+    void t2(){
+//        SimpleCalculator simpleCalculator = new SimpleCalculator();
         int rs = simpleCalculator.Plus(1,2);
         assertThat(rs).isEqualTo(3);
         rs = simpleCalculator.Plus(5,187);
         assertThat(rs).isEqualTo(192);
         rs = simpleCalculator.Plus(392,88);
         assertThat(rs).isEqualTo(480);
+    }
+
+    @Test
+    @DisplayName("곱하기 기능 5*8 = 40")
+    void t3(){
+        int rs = simpleCalculator.Multiply(5,8);
+        assertThat(rs).isEqualTo(40);
     }
 }
